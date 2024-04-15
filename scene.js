24556b21cdcdbd10ebf86a19eae1f8ed5fc419fb
Luma Oliveira <111361940+LumaOlli@users.noreply.github.com>
2024-04-15 14:24:12 -0300

    Sombra

--- scene.js
+++ scene.js
@@ -1,51 +1,84 @@
-function init() {
-  const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+//import * as THREE from 'three';
 
-  const renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-  //Redimensionando conteudo para caber em tela pequena
-  window.addEventListener("resize", function () {
-    var width = window.innerWidth;
-    var height = window.innerHeight;
-    renderer.setSize(width, height);
-    camera.aspect = width / height;
-    camera.updateProjectMatrix;
-  });
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
 
-  //Movimentação da camera
-  var controleMouse = new THREE.OrbitControls(camera, renderer.domElement);
-  controleMouse.update();
+//Redimensionando conteudo para caber em tela pequena
+window.addEventListener("resize", function () {
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+  renderer.setSize(width, height);
+  camera.aspect = width / height;
+  camera.updateProjectMatrix;
+});
 
-  // Luz direcional do tipo sombra
-  const luz1 = new THREE.DirectionalLight(0xffffff, 1);
-  luz1.position.set(0, 1, 0);
-  luz1.castShadow = true;
+//Movimentação da camera
+var controleMouse = new THREE.OrbitControls(camera, renderer.domElement);
+controleMouse.update();
 
-  // Configurações de sombra
-  luz1.shadow.mapSize.width = 1024;
-  luz1.shadow.mapSize.height = 1024;
-  luz1.shadow.camera.near = 0.5;
-  luz1.shadow.camera.far = 500;
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
+document.body.appendChild( renderer.domElement );
 
-  scene.add(luz1);
+//Create a PointLight and turn on shadows for the light
+const light = new THREE.PointLight( 0xffffff, 4, 100 );
+light.position.set( 5, 5, 7 );
+light.castShadow = true; // default false
+scene.add( light );
 
-  //Luz ambiente
-  const luz2 = new THREE.HemisphereLight(0xffffff, 0xff0000, 10);
-  scene.add(luz2);
+//Set up shadow properties for the light
+light.shadow.mapSize.width = 1024; // default
+light.shadow.mapSize.height = 1024; // default
+light.shadow.camera.near = 0.5; // default
+light.shadow.camera.far = 500; // default
 
-  let maquina = new THREE.Object3D();
-  const loader = new THREE.GLTFLoader();
+//fazer assim o SKYBOX!!!
+//Create a sphere that cast shadows (but does not receive them)
+const sphereGeometry = new THREE.SphereGeometry( 5, 32, 32 );
+const sphereMaterial = new THREE.MeshStandardMaterial( { color: 0xff0000 } );
+const sphere = new THREE.Mesh( sphereGeometry, sphereMaterial );
+sphere.castShadow = true; //default is false
+sphere.receiveShadow = false; //default
+//scene.add( sphere );
 
-  loader.load(
+//Luz ambiente
+const luz2 = new THREE.HemisphereLight(0xffffff, 0xff0000, 0.01);
+scene.add(luz2);
+
+//Create a plane that receives shadows (but does not cast them)
+const planeGeometry = new THREE.PlaneGeometry( 20, 20, 32, 32 );
+//const planeMaterial = new THREE.MeshStandardMaterial( { color: 0x00ff00 } )
+const planeMaterial = new THREE.MeshStandardMaterial({ map: new THREE.TextureLoader().load( 'img/Imagem_bottom.jpeg' ), side: THREE.DoubleSide }) //down side
+const plane = new THREE.Mesh( planeGeometry, planeMaterial );
+plane.receiveShadow = true;
+scene.add( plane );
+
+plane.rotation.x = -Math.PI/2;
+plane.position.y = -0.04;
+
+//Create a helper for the shadow camera (optional)
+const helper = new THREE.CameraHelper( light.shadow.camera );
+scene.add( helper );
+
+camera.position.z = 5;
+camera.position.y = 2
+
+let maquina = new THREE.Object3D();
+const loader = new THREE.GLTFLoader();
+
+let t = 0;
+
+loader.load(
 
     "locomotive/maquinac.gltf",
 
     function (maquinaScene) {
       maquina = maquinaScene.scene.children[0];
       maquinaScene.scene.children[0].scale.set(2, 2, 2);
+      maquina.castShadow = true;
       scene.add(maquinaScene.scene);
     }
   );
@@ -57,11 +90,12 @@ function init() {
     function (trilhoScene) {
       trilho = trilhoScene.scene.children[0];
       trilhoScene.scene.children[0].scale.set(2, 2, 2);
+      trilho.castShadow = true;
       scene.add(trilhoScene.scene);
     }
   );
 
-  //Cria um cubo como parte do cenário(skybox)
+   //Cria um cubo como parte do cenário(skybox)
   const geometry1 = new THREE.BoxGeometry( 10, 10, 10 );
   var cubeMaterials = [ 
     new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load( "img/Imagem_back.jpeg" ), side: THREE.DoubleSide }), //back side
@@ -70,18 +104,14 @@ function init() {
     new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load( 'img/Imagem_bottom.jpeg' ), side: THREE.DoubleSide }), //down side
     new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load( 'img/Imagem_right.jpeg' ), side: THREE.DoubleSide }), //right side
     new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load( 'img/Imagem_left.jpeg' ), side: THREE.DoubleSide }) //left side
-];
-
+  ];
+ 
   var cubeMaterial1 = new THREE.MeshFaceMaterial( cubeMaterials );
   const cube = new THREE.Mesh( geometry1, cubeMaterial1 );
   cube.position.y += 4.95;
+  //cube.castShadow = true; //default is false
+  cube.receiveShadow = true; //default
   scene.add( cube );
-
-  //Afastado da cena para melhor visualização
-  camera.position.z = 5; 
-  camera.position.y += 2;
-
-  let t = 0;
 
   //Carregando áudio
   const walk = new THREE.AudioListener();
@@ -119,6 +149,41 @@ function init() {
     }
   });
 
+// Variável para rastrear o modo de visualização da câmera
+let topViewMode = false;
+
+// Ativa quando uma tecla é pressionada
+window.addEventListener("keydown", function (event) {
+  switch (event.key) {
+    case "ArrowUp":
+      camera.position.z -= 0.1;
+      break;
+    case "ArrowDown":
+      camera.position.z += 0.1;
+      break;
+    case "ArrowLeft":
+      camera.position.x -= 0.1;
+      break;
+    case "ArrowRight":
+      camera.position.x += 0.1;
+      break;
+    case "c":
+    case "C":
+      if (topViewMode) {
+        // Volta para a câmera normal
+        camera.position.set(0, 2, 5);
+        camera.lookAt(0, 0, 0); // Mantém o foco no centro da cena
+        topViewMode = false;
+      } else {
+        // Alterna para a visão superior
+        camera.position.set(0, 10, 0); // Posição da câmera para a visão superior
+        camera.lookAt(0, 0, 0); // Foca no centro da cena
+        topViewMode = true;
+      }
+      break;
+  }
+});
+
   let isStopKeyPressed = true; // Inicialmente, o trem está parado
 
   // Ativa quando uma tecla é pressionada
@@ -134,44 +199,37 @@ function init() {
       }
     }
   });
-  
+
   function animate() {
     requestAnimationFrame(animate);
 
 
-      maquina.rotation.y = -(t - Math.PI/2);
-      
-      if (maquina) {
+    maquina.rotation.y = -(t - Math.PI/2);
+    
+    if (maquina) {
         // Raio do círculo
         const radius = 0.0001; 
         // Velocidade de rotação
         const speed = 0.01; 
-  
+
         // Calcula a posição do trem ao longo do círculo
         const x = radius * Math.cos(t);
         const z = radius * Math.sin(t);
-  
+
         // Define a posição do trem
         maquina.position.set(x, 0, z);
-  
+
         // Incrementa o ângulo
         if (!isStopKeyPressed) {
-          t += speed;
+        t += speed;
         }
         // Mantém o ângulo dentro do intervalo [0, 2 * PI]
         if (t > 2 * Math.PI) {
-          t -= 2 * Math.PI;
+        t -= 2 * Math.PI;
         }
-      }
+    }
     
-
-    renderer.render(scene, camera);
-  }
-  
-  animate();
+  renderer.render(scene, camera);
 }
 
-//Inicia a função 'init' após o carregamento completo do DOM
-document.addEventListener("DOMContentLoaded", function (event) {
-  init();
-});
+animate();
